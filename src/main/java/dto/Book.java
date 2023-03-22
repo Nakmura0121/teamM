c9b933b1d9102b6dfc65a98de835cc3188307129
t.nakamura <t.nakamura.sys22@morijyobi.ac.jp>
2023-03-22 12:34:24 +0900

    Book.dtoの作成

--- src/main/java/dto/Book.java
+++ src/main/java/dto/Book.java
@@ -1,5 +1,80 @@
 package dto;
 
 public class Book {
+	private int book_id;
+	private String name;
+	private String human;
+	private String publisher;
+	private String date;
+	private int isbn;
+	private String version;
+	
+	public Book(int book_id, String name, String human, String publisher, String date, int isbn, String version) {
+		super();
+		this.book_id = book_id;
+		this.name = name;
+		this.human = human;
+		this.publisher = publisher;
+		this.date = date;
+		this.isbn = isbn;
+		this.version = version;
+	}
 
+	public int getBook_id() {
+		return book_id;
+	}
+
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHuman() {
+		return human;
+	}
+
+	public void setHuman(String human) {
+		this.human = human;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
 }
